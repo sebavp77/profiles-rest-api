627feae42115902ffae41ed592a58b524483e451
Sebastian_Vergara_Palacio <sebasvpalacio7@gmail.com>
2022-05-24 22:42:23 +0200

    Patch to setup

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -22,7 +22,7 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 mkdir -p $VIRTUALENV_BASE_PATH
 python3 -m venv $VIRTUALENV_BASE_PATH/profiles_api
 
-$VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt
+$VIRTUALENV_BASE_PATH/profiles_api/bin/pip install -r $PROJECT_BASE_PATH/profiles-rest-api/requirements.txt
 
 # Run migrations
 cd $PROJECT_BASE_PATH/profiles-rest-api/src
